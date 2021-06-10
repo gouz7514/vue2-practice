47ad70d44f47d66e99eeb15da4fe3cee2f63efb3
gouz7514 <gouz7514@gmail.com>
2021-06-10 11:59:57 +0900

    update computed property practice

--- net-ninja/app.js
+++ net-ninja/app.js
@@ -2,41 +2,20 @@ new Vue({
   el: '#vue-app', // 어떤 element에 연결될지
   data: {
     age: 26,
-    x: 0,
-    y: 0
+    a: 0,
+    b: 0
   },
-  methods: {
-    // this는 Vue 인스턴스 자체
-    // data 안의 모든 속성들이 인스턴스에 종속된다.
-    add: function(x) {
-      this.age += x
+  // methods: {
+    
+  // },
+  computed: {
+    addToA: function() {
+      console.log('addToA run')
+      return this.a + this.age
     },
-    subtract: function(x) {
-      this.age -= x
-    },
-    updateXY: function(e) {
-      this.x = e.offsetX
-      this.y = e.offsetY
-    },
-    click: function() {
-      alert('you clicked me')
-    }
-  }
-})
-
-new Vue({
-  el: '#vue-key',
-  data: {
-    name: '',
-    age: ''
-  },
-  methods: {
-    logName: function() {
-      console.log('you entered name')
-    },
-    logAge: function() {
-      console.log('you entered age')
-
+    addToB : function() {
+      console.log('addToB run')
+      return this.b + this.age
     }
   }
 })
