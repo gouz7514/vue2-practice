271f8e3e976e815c3aed98c3df4f59b230904d44
gouz7514 <gouz7514@gmail.com>
2021-06-10 15:32:16 +0900

    update list rendering practice with v-for

--- net-ninja/app.js
+++ net-ninja/app.js
@@ -1,8 +1,12 @@
 new Vue({
   el: '#vue-app', // 어떤 element에 연결될지
   data: {
-    error: false,
-    success: false
+    characters: ['Mario', 'Luigi', 'Yoshi', 'Bowser'],
+    ninjas: [
+      {name: 'Ryu', age: 25},
+      {name: 'Yoshi', age: 35},
+      {name: 'Ken', age: 55}
+    ]
   },
   methods: {
     
