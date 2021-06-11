62143467ee050e5e2eb33192baace2f1f87ef9b5
gouz7514 <gouz7514@gmail.com>
2021-06-11 11:52:56 +0900

    create Vue CLI practice folder

---
+++ vue-cli-practice/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>{{title}}</h1>
+    <p>{{greeting()}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      title: "Hello World!"
+    }
+  },
+  methods: {
+    greeting: function() {
+      return 'greeting!'
+    }
+  },
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
