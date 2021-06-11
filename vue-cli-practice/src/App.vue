f2241c822b53f02158958dcee4abe8341a8b44f6
gouz7514 <gouz7514@gmail.com>
2021-06-11 14:20:43 +0900

    practice Props

--- vue-cli-practice/src/App.vue
+++ vue-cli-practice/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <app-header></app-header>
-    <kim></kim>
+    <kim v-bind:ninjas="ninjas"></kim>
     <app-footer></app-footer>
   </div>
 </template>
@@ -14,7 +14,14 @@ import Kim from './components/Kim.vue'
 export default {
   data() {
     return {
-      
+      ninjas: [
+        {name: 'Ryu', speciality: 'Vue Components', show: false},
+        {name: 'Crystal', speciality: 'HTML Wizardry', show: false},
+        {name: 'Hitoshi', speciality: 'Click Events', show: false},
+        {name: 'Tango', speciality: 'Conditionals', show: false},
+        {name: 'Kami', speciality: 'Webpack', show: false},
+        {name: 'Yoshi', speciality: 'Data Diggin', show: false}
+      ]
     }
   },
   components: {
