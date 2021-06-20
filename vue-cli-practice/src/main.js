f90b80c63c44e3f04bbcec98625a86e55e4d69cd
gouz7514 <gouz7514@gmail.com>
2021-06-20 15:56:29 +0900

    practice Custom Directives

--- vue-cli-practice/src/main.js
+++ vue-cli-practice/src/main.js
@@ -3,6 +3,27 @@ import App from './App.vue'
 
 export const bus = new Vue(); // event bus
 
+// custom directives
+Vue.directive('rainbow', {
+  bind(el) {
+    el.style.color = "#" + Math.random().toString().slice(2,8);
+  }
+})
+
+Vue.directive('theme', {
+  bind(el, binding) {
+    if (binding.value == 'wide') {
+      el.style.maxWidth = "1200px"
+    } else if (binding.value == 'narrow') {
+      el.style.maxWidth = "600px"
+    }
+    if (binding.arg == 'column') {
+      el.style.background = '#ddd'
+      el.style.padding = '20px'
+    }
+  }
+})
+
 // root vue instance
 new Vue({
   el: '#app',
