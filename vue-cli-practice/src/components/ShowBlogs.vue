73bb8ce3232f0c1841e388861f783a72845fd549
gouz7514 <gouz7514@gmail.com>
2021-06-22 10:26:50 +0900

    practice custom search filter

--- vue-cli-practice/src/components/ShowBlogs.vue
+++ vue-cli-practice/src/components/ShowBlogs.vue
@@ -1,7 +1,8 @@
 <template>
   <div v-theme:column="'narrow'" id="show-blogs">
     <h1>All Blog Articles</h1>
-    <div v-for="(blog,idx) in blogs" :key="idx" class="single-blog">
+    <input type="text" v-model="search" placeholder="search blogs"/>
+    <div v-for="(blog,idx) in filteredBlogs" :key="idx" class="single-blog">
       <h2 v-rainbow>{{blog.title | to-uppercase}}</h2>
       <article>{{blog.body | snippet}}</article>
     </div>
@@ -17,7 +18,8 @@ export default {
   },
   data() {
     return {
-      blogs: []
+      blogs: [],
+      search: ""
     }
   },
   methods: {
@@ -26,6 +28,13 @@ export default {
   created() {
     axios.get('http://jsonplaceholder.typicode.com/posts')
       .then((data) => this.blogs = data.data.slice(0,10))
+  },
+  computed: {
+    filteredBlogs: function() {
+      return this.blogs.filter((blog) => {
+        return blog.title.match(this.search)
+      })
+    }
   }
 }
 </script>
