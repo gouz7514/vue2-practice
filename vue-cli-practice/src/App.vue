17071244b51c95a427000f611addf626194b61a1
gouz7514 <gouz7514@gmail.com>
2021-06-25 10:04:39 +0900

    practice Mixins

--- vue-cli-practice/src/App.vue
+++ vue-cli-practice/src/App.vue
@@ -2,17 +2,20 @@
   <div>
     <!-- <add-blogs></add-blogs> -->
     <show-blogs></show-blogs>
+    <list-blogs></list-blogs>
   </div>
 </template>
 
 <script>
 // import AddBlogs from './components/AddBlogs.vue'
 import ShowBlogs from './components/ShowBlogs.vue'
+import listBlogs from './components/listBlogs.vue'
 
 export default {
   components: {
     // 'add-blogs' : AddBlogs,
-    'show-blogs': ShowBlogs
+    'show-blogs': ShowBlogs,
+    'list-blogs': listBlogs
   },
   data() {
     return {
